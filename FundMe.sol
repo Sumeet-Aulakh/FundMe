edc26943f0e153c69e1ca55c20ccf8b160a21fac
Sumeet-Aulakh <sumeetbinjal@gmail.com>
2023-12-12 23:45:34 -0600

    Sending ETH

--- FundMe.sol
+++ FundMe.sol
@@ -5,9 +5,24 @@ pragma solidity ^0.8.18;
 contract FundMe {
     uint256 minimumUSD = 5;
 
+    address[] public funders;
+
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable {
         require(msg.value > minimumUSD, "Didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {}
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+    }
 }
